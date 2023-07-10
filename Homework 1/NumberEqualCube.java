286a64c8d4c2f684e35033097d754ee7927ceff2
architJoshi2904 <aj6082@rit.edu>
2023-07-10 15:56:05 +0530

    Homework 1.1 - NumberEqualCube

---
+++ Homework 1/NumberEqualCube.java
@@ -0,0 +1,90 @@
+/**
+ * NumberEqualCube.java
+ * Version:
+ *
+ * Revisions:
+ *
+ */
+
+/**
+ * This class calculates the sum of a given number n with length d. Each of the
+ * digits in 'n' are raised to the length of the number 'd' and their sum 's'
+ * is calculated. Output will be printed only if the sum 's' is equal to the
+ * original number 'n'.
+ * Example -
+ * 1.)  Assume n = 153, then n is a number which meets the property, because
+ *      153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27.
+ * 2.)  Assume n = 154, then n is a number which does not meet the property,
+ *      because 154 ≠ 1^3 + 5^3 + 4^3 = 190.
+ *
+ * @author      Archit Joshi
+ * @author      Cassie Clark
+ */
+
+public class NumberEqualCube {
+    /**
+     * Method to check number for described pattern.
+     */
+    private static void checkPattern() {
+        for ( int num = 1; num <= 10000; num++ ) {
+            int sum = 0;
+            int temp = num;
+            char[] numAsChar = ( "" + num ).toCharArray();
+            int len = numAsChar.length;
+            sum = sumOfNum(temp, len, sum);
+
+            if(sum == num){
+                printFormattedOutput(num, len, numAsChar);
+            }
+
+
+        }
+    }
+
+    /**
+     * Helper method to print the output in expected format
+     * @param num
+     * @param len
+     * @param numAsChar
+     */
+    private static void printFormattedOutput( int num, int len, char[] numAsChar ) {
+        System.out.print( "\n" + num + " = " );
+        for ( int j=0; j < len; j++ ) {
+            System.out.print( numAsChar[j] + "^" + len );
+
+            if ( j != len-1 ) {
+                System.out.print( " + " );
+            }
+        }
+    }
+
+    private static int sumOfNum(int temp, int len, int sum) {
+        while ( temp > 0 ) {
+            int rem = temp % 10;
+            int temp1 = rem;
+
+            // For loop to calculate the power of the individual digits in the
+            // original number. The loop runs for the length of the number
+            // which is stored in 'len'
+
+            for( int i = 1; i < len; i++ ) {
+                // The last digit stored in temp1 will be multiplied by rem
+                temp1 = temp1 * rem;
+            }
+
+            // Calculation for final
+            sum += temp1;
+
+            // Number is divided by 10 to get the quotient which removes the
+            // digit already worked on.
+            temp /= 10;
+        }
+
+        //Return the value of sum to the main driver program
+        return sum;
+    }
+
+    public static void main ( String[] args ) {
+        checkPattern();
+    }
+}
